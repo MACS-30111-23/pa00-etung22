4e54c6dacfa7128a1d168526a3d42e9bd0b66399
etung22 <etung22@uchicago.edu>
2024-10-01 08:47:57 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
-# This file is intentionally empty
+#All about Emma Tung
+*Emma Tung is from Bay Area, California, and she enjoys playing tennis, running, cooking, and eating KBBQ*
+##some fun facts about Emma
+1. Emma has tried all of Ben and Jerry's ice cream flavors (best is half baked)
+2. Her brother is in the year below at UChicago
+3. Emma is originally from Hong Kong, but enjoys the Bay Area
+4. Emma loves Boston too as she went to high school on the East Coast
+5. Emma has a dog that is now being taken care of by mom and dad
+6. Emma wishes to join the sailing team# This file is intentionally empty
 #
 # add your code for printing out a message, such as 'Hello World!' or something else!
